7dc7790f2bc47c7591b3d36d073652582f7d3538
Kalbin Vladimir <kalbin.wow@yandex.ru>
2023-06-04 19:31:55 +0500

    Code refactoring

--- PackagesHandlers.py
+++ PackagesHandlers.py
@@ -9,12 +9,10 @@ from StorageServer import StorageServer
 def handle_package(package: Package, host_socket: socket.socket, server: StorageServer) -> None:
     match package.get_type():
         case PackageType.HOST_CONNECT_REQUEST:
-            host_connect_request(package, host_socket, server)
-        case PackageType.CONNECTION_LOST:
-            pass
+            handle_host_connect_request(package, host_socket, server)
 
 
-def host_connect_request(package: Package, host_socket: socket.socket, server: StorageServer) -> None:
+def handle_host_connect_request(package: Package, host_socket: socket.socket, server: StorageServer) -> None:
     host_addr = host_socket.getpeername()
 
     logging.info(f'Host {host_addr} connected!')
@@ -24,10 +22,4 @@ def host_connect_request(package: Package, host_socket: socket.socket, server: S
     successful_connect_response = ConnectionResponsePackage()
 
     successful_connect_response.send(host_socket)
-
-
-def host_connect_lost(package: Package, host_socket: socket.socket, sever: StorageServer) -> None:
-    host_addr = host_socket.getpeername()
-
-    logging.info(f'Host {host_addr} disconnecting...')
 
