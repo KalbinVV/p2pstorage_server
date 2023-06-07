3a516e7c5ddd54f583e82b10fbdcc5b4ceb46c92
Kalbin Vladimir <kalbin.wow@yandex.ru>
2023-06-07 22:39:53 +0500

    Columns should not be uniques

--- db/sqls/create_files_table.sql
+++ db/sqls/create_files_table.sql
@@ -1,8 +1,8 @@
 create table if not exists files(
     id integer primary key autoincrement,
     host_id integer not null,
-    name str not null unique,
+    name str not null,
     size integer not null,
-    hash str not null unique,
+    hash str not null,
     foreign key(host_id) references hosts(id) on delete cascade
 )
