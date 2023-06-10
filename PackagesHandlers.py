9525cc0ca6e9ddb9c02ec03078879826bbe568f6
Kalbin Vladimir <kalbin.wow@yandex.ru>
2023-06-10 19:31:21 +0500

    Major transactions rework.

--- PackagesHandlers.py
+++ PackagesHandlers.py
@@ -1,5 +1,6 @@
 import logging
 import socket
+from time import sleep
 
 from p2pstorage_core.server.Host import Host, HostInfo
 import p2pstorage_core.server.Package as Pckg
@@ -8,6 +9,7 @@ import Configuration
 from StorageServer import StorageServer
 
 
+# TODO: Refactor this, maybe dict
 def handle_package(package: Pckg.Package, host_socket: socket.socket, server: StorageServer) -> None:
     match package.get_type():
         case Pckg.PackageType.HOST_CONNECT_REQUEST:
@@ -20,8 +22,10 @@ def handle_package(package: Pckg.Package, host_socket: socket.socket, server: St
             handle_files_list_request(host_socket, server)
         case Pckg.PackageType.GET_FILE_BY_ID_REQUEST:
             handle_get_file_by_id_request(package, host_socket, server)
+        case Pckg.PackageType.FILE_TRANSACTION_START_RESPONSE:
+            handle_transaction_start_response(package, host_socket, server)
         case Pckg.PackageType.TRANSACTION_FINISHED:
-            handle_transaction_finished(package, server)
+            handle_transaction_finished_response(host_socket, server)
 
 
 def handle_host_connect_request(package, host_socket: socket.socket, server: StorageServer) -> None:
@@ -89,5 +93,67 @@ def handle_get_file_by_id_request(package: Pckg.Package, host_socket: socket.soc
         get_file_by_id_response = Pckg.FileTransactionStartResponsePackage(transaction_started=False,
                                                                            file_name='',
                                                                            reject_reason='File not exists!',
-                                                                           sender_addr=None)
+                                                                           sender_addr=None,
+                                                                           receiver_addr=None)
         get_file_by_id_response.send(host_socket)
+
+        return
+
+    file_name = files_manager.get_file_by_id(file_id).name
+
+    hosts_manager = server.get_hosts_manager()
+
+    transactions_manager = server.get_transactions_manager()
+
+    host_addr = host_socket.getpeername()
+
+    transaction_was_started = False
+
+    for another_host in hosts_manager.get_hosts():
+        # if another_host.host_socket.getpeername() == host_socket.getpeername():
+        # continue
+
+        peer_name = another_host.host_socket.getpeername()
+
+        transaction_start_request = Pckg.FileTransactionStartRequestPackage(file_name,
+                                                                            establish_addr=peer_name,
+                                                                            receiver_addr=host_addr)
+        transaction_start_request.send(another_host.host_socket)
+
+        # Waiting response from another host
+        sleep(1)
+
+        if transactions_manager.is_transaction_was_started(host_addr):
+            transaction_was_started = True
+            break
+
+    if not transaction_was_started:
+        transaction_start_response = Pckg.FileTransactionStartResponsePackage(None,
+                                                                              None,
+                                                                              file_name,
+                                                                              False,
+                                                                              'File not exists!')
+        transaction_start_response.send(host_socket)
+
+
+def handle_transaction_start_response(package: Pckg.Package, _host_socket: socket.socket,
+                                      server: StorageServer) -> None:
+    transaction_start_response = Pckg.FileTransactionStartResponsePackage.from_abstract(package)
+
+    transactions_manager = server.get_transactions_manager()
+
+    receiver_addr = transaction_start_response.get_receiver_addr()
+    receiver_host = server.get_hosts_manager().get_host_by_addr(receiver_addr)
+
+    if transaction_start_response.is_transaction_started():
+        transactions_manager.set_transaction_to_started(receiver_addr, True)
+
+        transaction_start_response.send(receiver_host.host_socket)
+
+
+def handle_transaction_finished_response(host_socket: socket.socket, server: StorageServer) -> None:
+    receiver_addr = host_socket.getpeername()
+
+    server.get_transactions_manager().set_transaction_to_started(receiver_addr, False)
+
+    logging.info(f'[Transaction] {receiver_addr} finished transaction!')
