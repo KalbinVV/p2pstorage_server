7aa87ddb940c32174f1b4cd59793f4ea6231250e
Kalbin Vladimir <kalbin.wow@yandex.ru>
2023-06-15 19:20:25 +0500

    Ownership rework

--- packages/handlers/callbacks/TransactionFinishedResponse.py
+++ packages/handlers/callbacks/TransactionFinishedResponse.py
@@ -1,7 +1,7 @@
 import logging
 import socket
 
-from p2pstorage_core.server.Package import Package, PackageType
+from p2pstorage_core.server.Package import Package, PackageType, FileTransactionFinishedPackage
 
 from StorageServer import StorageServer
 from packages.handlers.PackageCallback import AbstractPackageCallback
@@ -27,3 +27,13 @@ class TransactionFinishedResponse(AbstractPackageCallback):
         server.get_transactions_manager().set_transaction_to_started(receiver_addr, False)
 
         logging.info(f'[Transaction] {receiver_addr} finished transaction!')
+
+        files_manager = server.get_files_manager()
+        hosts_manager = server.get_hosts_manager()
+
+        file_name = FileTransactionFinishedPackage.from_abstract(package).get_filename()
+
+        host_id = hosts_manager.get_host_id_by_addr(receiver_addr)
+        file_id = files_manager.get_file_id_by_name(file_name)
+
+        files_manager.add_file_owner(file_id, host_id)
